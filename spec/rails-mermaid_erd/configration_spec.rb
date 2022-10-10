a2252009cf7dac10b7a1067582039d762e6d21f2
💩 <69432552+unchidev@users.noreply.github.com>
2022-10-11 02:31:06 +0900

    Add support for custom configuration

---
+++ spec/rails-mermaid_erd/configration_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe RailsMermaidErd::Configuration do
+  let(:configuration) { RailsMermaidErd::Configuration.new }
+
+  context "when the config file does not exist" do
+    it "return default config value" do
+      expect(configuration.result_path).to eq('mermaid_erd/index.html')
+    end
+  end
+
+  context "when the config file exist" do
+    before do
+      FileUtils.cp(file_fixture("configurations/valid.yml").to_s, Rails.root.join("config/mermaid_erd.yml").to_s)
+    end
+    after do
+      FileUtils.rm(Rails.root.join("config/mermaid_erd.yml").to_s)
+    end
+    it "return overwrite config value" do
+      expect(configuration.result_path).to eq('doc/erd.html')
+    end
+  end
+end
