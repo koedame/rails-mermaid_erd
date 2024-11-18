f8eddc0f716aa0d644cf861fcce72751909c99d8
Francesco <francescob@gmail.com>
2024-11-18 15:05:06 +0100

    added option to specify a different config file

--- lib/rails-mermaid_erd/configuration.rb
+++ lib/rails-mermaid_erd/configuration.rb
@@ -5,14 +5,16 @@ class RailsMermaidErd::Configuration
   attr_accessor :models_list
   attr_accessor :hide_columns
 
-  def initialize
+  def initialize(config_file)
+    config_file = config_file || "config/mermaid_erd.yml"
     config = {
       result_path: "mermaid_erd/index.html",
       models_list: "::ActiveRecord::Base.descendants.sort_by(&:name)",
       hide_columns: false
     }
+    puts "config_file: #{config_file}"
+    config_file = Rails.root.join(config_file)
 
-    config_file = Rails.root.join("config/mermaid_erd.yml")
     if File.exist?(config_file)
       custom_config = YAML.load(config_file.read).symbolize_keys
       config = config.merge(custom_config)
@@ -21,5 +23,6 @@ class RailsMermaidErd::Configuration
     @result_path = config[:result_path]
     @models_list = config[:models_list]
     @hide_columns = config[:hide_columns]
+    puts "models_list: #{@models_list}"
   end
 end
